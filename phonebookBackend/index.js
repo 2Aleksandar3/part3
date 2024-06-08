d6e544a1f0e65ddaa5560a1bea0c1d5a265cb322
2Aleksandar3 <126813012+2Aleksandar3@users.noreply.github.com>
2024-06-08 11:25:51 +0200

    Add files via upload

--- phonebookBackend/index.js
+++ phonebookBackend/index.js
@@ -45,7 +45,7 @@ app.post('/api/persons', (request, response) => {
   const people =new Person( {
     name: body.name,
     number: body.number,
-    id: Math.floor(Math.random() * (99999 - persons.length + 1)) + persons.length,
+    id: Math.floor(Math.random() * (99999 - Person.length + 1)) + Person.length,
   })
   
   console.log(people.id)
